b26cbf7aeeceed1c2e7693ef3793eaaaf486f70c
calysstaa <calystacayla03@gmail.com>
2026-01-15 17:23:07 +0700

    Update article.php

--- article.php
+++ article.php
@@ -1,20 +1,32 @@
 <?php
 include "koneksi.php";
-$search = isset($_POST['search']) ? $_POST['search'] : '';
+
+$search = "";
+if (isset($_POST['search'])) {
+    $search = $_POST['search'];
+}
+
 $query = "SELECT * FROM article WHERE judul LIKE '%$search%' ORDER BY tanggal DESC";
 $result = $conn->query($query);
 
-while($row = $result->fetch_assoc()){
-    ?>
-    <div class="col">
-        <div class="card h-100">
-            <img src="img/<?= $row['gambar'] ?>" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title"><?= $row['judul'] ?></h5>
-                <p class="card-text"><?= $row['isi'] ?></p>
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        ?>
+        <div class="col">
+            <div class="card h-100 shadow-sm">
+                <img src="img/<?php echo $row['gambar']; ?>" class="card-img-top" alt="Image">
+                <div class="card-body text-start">
+                    <h5 class="card-title"><?php echo $row['judul']; ?></h5>
+                    <p class="card-text text-muted"><?php echo substr($row['isi'], 0, 100); ?>...</p>
+                </div>
+                <div class="card-footer bg-white border-top-0">
+                    <small class="text-body-secondary">Diposting: <?php echo $row['tanggal']; ?></small>
+                </div>
             </div>
         </div>
-    </div>
-    <?php
+        <?php
+    }
+} else {
+    echo "<p class='text-center'>Tidak ada artikel ditemukan.</p>";
 }
 ?>

7b08355abe24d34373fa8a7b22f8fbd76ba89b0c
calysstaa <calystacayla03@gmail.com>
2026-01-15 17:17:13 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -7,6 +7,6 @@ $db   = "webdailyjournal";
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Koneksi gagal: " . $conn->connect_error);
 }
 ?>

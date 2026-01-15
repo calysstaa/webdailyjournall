564f7ccabbbefee5368f03bcc088732bf8bd0e05
calysstaa <calystacayla03@gmail.com>
2026-01-15 18:03:17 +0700

    Update profile_upload.php

--- profile_upload.php
+++ profile_upload.php
@@ -5,18 +5,20 @@ include "koneksi.php";
 if (isset($_POST['simpan_profile'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    
+    $foto_baru = $_FILES['foto']['name'];
+
     if (!empty($password)) {
         $password_fix = md5($password);
         $conn->query("UPDATE user SET password = '$password_fix' WHERE username = '$username'");
     }
 
-    if ($_FILES['foto']['name'] != "") {
-        $nama_foto = time() . "_" . $_FILES['foto']['name'];
-        move_uploaded_file($_FILES['foto']['tmp_name'], "img/" . $nama_foto);
-        $conn->query("UPDATE user SET foto = '$nama_foto' WHERE username = '$username'");
+    if ($foto_baru != "") {
+        $ext = pathinfo($foto_baru, PATHINFO_EXTENSION);
+        $nama_file = $username . "_" . time() . "." . $ext;
+        move_uploaded_file($_FILES['foto']['tmp_name'], "img/" . $nama_file);
+        $conn->query("UPDATE user SET foto = '$nama_file' WHERE username = '$username'");
     }
 
-    echo "<script>alert('Profil Berhasil Diperbarui'); window.location='admin.php?page=profile';</script>";
+    echo "<script>alert('Profil berhasil diperbarui!'); window.location='admin.php?page=profile';</script>";
 }
 ?>

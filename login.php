f01207e14246f1cc953d4574c38a3b9e12c4d0ed
calysstaa <calystacayla03@gmail.com>
2026-01-15 17:18:04 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+if (isset($_POST['login'])) {
+    $username = $_POST['user'];
+    $password = md5($_POST['pass']);
+
+    $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['username'] = $username;
+        header("location:admin.php");
+    } else {
+        echo "<script>alert('Username atau Password Salah!');</script>";
+    }
+}
+?>
+<form method="POST">
+    <input type="text" name="user" placeholder="Username" required>
+    <input type="password" name="pass" placeholder="Password" required>
+    <button type="submit" name="login">Login</button>
+</form>

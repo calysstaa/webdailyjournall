45de64b967f4b5a141739ce1c7cbbd6406e66e4c
calysstaa <calystacayla03@gmail.com>
2026-01-15 17:53:48 +0700

    Enhance article card layout with footer and text length
    Updated the card display to include a footer with the date and adjusted text truncation.

--- article.php
+++ article.php
@@ -8,13 +8,14 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo '
         <div class="col">
-            <div class="card h-100 shadow-sm text-start">
-                <img src="img/'.$row['gambar'].'" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title">'.$row['judul'].'</h5>
-                    <p class="card-text">'.substr($row['isi'], 0, 80).'...</p>
-                </div>
+          <div class="card h-100 shadow-sm">
+            <img src="img/'.$row['gambar'].'" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">'.$row['judul'].'</h5>
+              <p class="card-text">'.substr($row['isi'], 0, 100).'...</p>
             </div>
+            <div class="card-footer small text-muted">'.$row['tanggal'].'</div>
+          </div>
         </div>';
     }
 } else {

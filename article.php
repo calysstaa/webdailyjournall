3b9dc8ffc5902c9bbd9dda019cb02f3de697b995
calysstaa <calystacayla03@gmail.com>
2026-01-15 17:40:57 +0700

    Refactor search handling and update article display

--- article.php
+++ article.php
@@ -1,32 +1,23 @@
 <?php
 include "koneksi.php";
-
-$search = "";
-if (isset($_POST['search'])) {
-    $search = $_POST['search'];
-}
-
+$search = $_POST['search'] ?? '';
 $query = "SELECT * FROM article WHERE judul LIKE '%$search%' ORDER BY tanggal DESC";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        ?>
+        echo '
         <div class="col">
-            <div class="card h-100 shadow-sm">
-                <img src="img/<?php echo $row['gambar']; ?>" class="card-img-top" alt="Image">
-                <div class="card-body text-start">
-                    <h5 class="card-title"><?php echo $row['judul']; ?></h5>
-                    <p class="card-text text-muted"><?php echo substr($row['isi'], 0, 100); ?>...</p>
-                </div>
-                <div class="card-footer bg-white border-top-0">
-                    <small class="text-body-secondary">Diposting: <?php echo $row['tanggal']; ?></small>
+            <div class="card h-100 shadow-sm text-start">
+                <img src="img/'.$row['gambar'].'" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">'.$row['judul'].'</h5>
+                    <p class="card-text">'.substr($row['isi'], 0, 80).'...</p>
                 </div>
             </div>
-        </div>
-        <?php
+        </div>';
     }
 } else {
-    echo "<p class='text-center'>Tidak ada artikel ditemukan.</p>";
+    echo "<p class='text-center w-100'>Data tidak ditemukan.</p>";
 }
 ?>

8d5a11be5b4a1d602a4e71150ed735084da24269
calysstaa <calystacayla03@gmail.com>
2026-01-15 17:19:42 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -4,6 +4,20 @@ include 'koneksi.php';
 if (!isset($_SESSION['username'])) { header("location:login.php"); }
 
 if (isset($_POST['simpan'])) {
+    $judul = $_POST['judul'];
+    $isi = $_POST['isi'];
+    $gambar = $_FILES['gambar']['name'];
+    
+    if ($gambar != "") {
+        move_uploaded_file($_FILES['gambar']['tmp_name'], "img/".$gambar);
+    }
+    
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+        $conn->query("UPDATE article SET judul='$judul', isi='$isi', gambar='$gambar' WHERE id='$id'");
+    } else {
+        $conn->query("INSERT INTO article (judul, isi, gambar, tanggal) VALUES ('$judul', '$isi', '$gambar', NOW())");
+    }
 }
 
 if (isset($_GET['hapus'])) {
